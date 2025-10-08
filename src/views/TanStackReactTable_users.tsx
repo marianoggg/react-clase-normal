d81fac6bfe68d58bd7484af0b878163efb366fd8
marianoggg <mmprincic@gmail.com>
2025-10-07 22:34:30 -0300

    clase tanStack trayendo datos de usuarios del bknd

--- src/views/TanStackReactTable_users.tsx
+++ src/views/TanStackReactTable_users.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import {
   useReactTable,
   getCoreRowModel,
@@ -20,38 +20,49 @@ type User = {
 
 type Props = {};
 
-const defaultData: any[] = [
-  { name: "juan", age: 22 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-  { name: "pedro", age: 23 },
-];
-
 const defaultColumns = [
   {
-    header: "Nombres",
-    accessorKey: "name",
+    header: "Nombre",
+    accessorKey: "first_name",
   },
   {
-    header: "Edades",
-    accessorKey: "age",
+    header: "Apellido",
+    accessorKey: "last_name",
+  },
+  {
+    header: "Email",
+    accessorKey: "email",
+  },
+  {
+    header: "Type",
+    accessorKey: "type",
+    cell: (info: any) => (
+      <span style={{ color: info.getValue() === "admin" ? "red" : "black" }}>
+        {info.getValue()}
+      </span>
+    ),
   },
 ];
 
 function TanStackReactTableUsers({}: Props) {
   //logica
+
+  const BACKEND_IP = "localhost";
+  const BACKEND_PORT = "8000";
+  const ENDPOINT = "user/paginated/filtered-sync";
+  const URL = `http://${BACKEND_IP}:${BACKEND_PORT}/${ENDPOINT}`;
+
   const [globalFilter, setGlobalFilter] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<User[]>([]);
+  const [nextCursor, setNextCursor] = useState<number | null>(null);
+  const [cursor, setCursor] = useState<number>(0);
+  const [previousCursors, setPreviousCursors] = useState<number[]>([]);
 
   const tableContainerRef = useRef<HTMLDivElement>(null);
 
   const table = useReactTable({
-    data: defaultData,
+    data: data,
     columns: defaultColumns,
     state: {
       globalFilter,
@@ -62,6 +73,7 @@ function TanStackReactTableUsers({}: Props) {
   });
 
   const { rows } = table.getRowModel();
+
   const rowVirtualized = useVirtualizer({
     count: rows.length,
     getScrollElement: () => tableContainerRef.current,
@@ -70,6 +82,78 @@ function TanStackReactTableUsers({}: Props) {
   });
 
   const virtualRows = rowVirtualized.getVirtualItems();
+
+  const fetchUsers = async (
+    cursorId: number = 0,
+    isGoingBack: boolean = false
+  ) => {
+    setIsLoading(true);
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const res = await fetch(URL, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          limit: 20,
+          last_seen_id: cursorId,
+        }),
+      });
+
+      const obj = await res.json();
+
+      if (obj.message) {
+        console.log(
+          "Error obj traido del backend (trayendo usuarios) -->",
+          obj.message
+        );
+        return;
+      }
+
+      setData(obj.users);
+      setNextCursor(obj.next_cursor ?? null);
+
+      if (!isGoingBack && cursorId !== 0) {
+        setPreviousCursors((prev) => [...prev, cursor]);
+      }
+
+      setCursor(cursorId);
+    } catch (error) {
+      console.error("Fetch error al traer usuarios -> ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleNext = () => {
+    if (nextCursor !== null) {
+      fetchUsers(nextCursor);
+    }
+  };
+  const handlePrev = () => {
+    if (previousCursors.length > 0) {
+      const prevCur = previousCursors[previousCursors.length - 1];
+      const newPreviousCursors = previousCursors.slice(0, -1);
+      setPreviousCursors(newPreviousCursors);
+      console.log("prevCur", prevCur, previousCursors, newPreviousCursors);
+      fetchUsers(prevCur, true);
+    }
+  };
+
+  /*  previousCursor -> [0,20,40,60,80]
+
+  previousCursors -> [0,20,40,60] */
+
+  useEffect(() => {
+    fetchUsers(0);
+  }, []);
 
   //ui
   return (
@@ -111,6 +195,12 @@ function TanStackReactTableUsers({}: Props) {
             })}
           </tbody>
         </table>
+      </div>
+
+      {/* Controles de paginacion */}
+      <div>
+        <button onClick={handlePrev}>Anterior</button>
+        <button onClick={handleNext}>Siguiente</button>
       </div>
     </div>
   );
